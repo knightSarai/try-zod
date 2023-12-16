96e945a1cf598bff6c66f85747d8c59ebc97b161
knightSarai <knight.sarai.dev@gmail.com>
2023-12-16 19:55:04 +0300

    Add test unique user

--- libs/users/src/lib/users.int.spec.ts
+++ libs/users/src/lib/users.int.spec.ts
@@ -21,7 +21,20 @@ describe('test test', () => {
             });
 
         expect(res.status).toBe(200);
+        expect(res.body.id).toBeDefined();
+    });
 
+    it('should fail to create a user if user already exists', async () => {
+        const res = await request(app)
+            .post('/user')
+            .set('Accept', 'application/json')
+            .send({
+                name: 'John Doe',
+                email: 'tes@gmail.com',
+                password: '123'
+            });
+
+        expect(res.status).toBe(400);
     });
 
     it('should get users', async () => {
